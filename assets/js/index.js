e0922240be6b8f7c124176c050e8f7d050180788
Roman Revunenkov <romik.rev@gmail.com>
2020-08-27 22:29:23 +0300

    code formatter

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,80 +1,74 @@
 "use strict";
 
-
 // Tasks from lesson practics:
 
 class MyString {
   static reverce(string) {
-      return string.split("").reverce().join("");
+    return string.split("").reverce().join("");
   }
-  
+
   static ucFirst(string) {
-      return string
-        .split()
-        .map((latter) => `${latter.charAt(0).toUpperCase()}${latter.slice(1)}`)
-        .join();
+    return string
+      .split()
+      .map((latter) => `${latter.charAt(0).toUpperCase()}${latter.slice(1)}`)
+      .join();
   }
 
   static ucWords(string) {
-      return string
-        .split(" ")
-        .map((word) => `${word.charAt(0).toUpperCase()}${word.slice(1)}`)
-        .join(" ");
+    return string
+      .split(" ")
+      .map((word) => `${word.charAt(0).toUpperCase()}${word.slice(1)}`)
+      .join(" ");
   }
-
 }
-
-
-
 
 class User {
   constructor(name, surname) {
-      this._name = name;
-      this._surname = surname;
+    this._name = name;
+    this._surname = surname;
   }
 
   set name(name) {
-      if (typeof name !== "string") {
-        throw new TypeError("Name mast be a string");
-      }
-      if (name.length > 128 || name === "") {
-        throw new RangeError("Name must be within 1 to 128 symbols");
-      }
-      this._name = name;
+    if (typeof name !== "string") {
+      throw new TypeError("Name mast be a string");
     }
-  
-    get name() {
-      return this._name;
+    if (name.length > 128 || name === "") {
+      throw new RangeError("Name must be within 1 to 128 symbols");
     }
+    this._name = name;
+  }
 
-    set surname(surname) {
-      if (typeof surname !== "string") {
-        throw new TypeError("Surname mast be a string");
-      }
-      if (surname.length > 128 || surname === "") {
-        throw new RangeError("Surame must be within 1 to 128 symbols");
-      }
-      this._surname = surname;
-    }
-  
-    get sutname() {
-      return this._surname;
-    }
+  get name() {
+    return this._name;
+  }
 
-    get FullName() {
-      return `${this._name} ${this._surname}`;
+  set surname(surname) {
+    if (typeof surname !== "string") {
+      throw new TypeError("Surname mast be a string");
     }
+    if (surname.length > 128 || surname === "") {
+      throw new RangeError("Surame must be within 1 to 128 symbols");
+    }
+    this._surname = surname;
+  }
+
+  get sutname() {
+    return this._surname;
+  }
+
+  get FullName() {
+    return `${this._name} ${this._surname}`;
+  }
 }
 
 class Student extends User {
   constructor(name, surname, year) {
-      super(name, surname);
-      this._year = year;
+    super(name, surname);
+    this._year = year;
   }
 
-
-  get Course () {
-      const date = new Date(); 
-      return date.getFullYear() - this._year;
-    }
+  get Course() {
+    const date = new Date();
+    return date.getFullYear() - this._year;
+  }
 }
